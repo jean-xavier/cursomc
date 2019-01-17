9946ca988f3be4b6c8824eea05321d95d8354f80
Jean Xavier <jean_xavier_@outlook.com>
2019-01-17 11:07:51 -0300

    Adicionado banco de dados H2. Criação da base de dados

--- src/main/java/com/cursomc/cursomc/domain/Categoria.java
+++ src/main/java/com/cursomc/cursomc/domain/Categoria.java
@@ -1,11 +1,20 @@
 package com.cursomc.cursomc.domain;
 
 
-import java.io.Serializable;
-import java.util.Objects;
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Data
+@Entity
 public class Categoria implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
@@ -16,32 +25,4 @@ public class Categoria implements Serializable {
         this.nome = nome;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Categoria categoria = (Categoria) o;
-        return id.equals(categoria.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
