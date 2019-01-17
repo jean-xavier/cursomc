235f3a5df8e671ab3f0aa49a8f20a621eca680ab
Jean Xavier <jean_xavier_@outlook.com>
2019-01-17 10:43:56 -0300

    Adicionado a classe de dominio Categoria e testado end point categoria

--- src/main/java/com/cursomc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,33 @@
 package com.cursomc.cursomc.resources;
 
+import com.cursomc.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar() {
-        return "Rest is done!";
+    public List<Categoria> listar() {
+
+        Categoria cat1 = new Categoria();
+        cat1.setId(1L);
+        cat1.setNome("Informática");
+
+        Categoria cat2 = new Categoria();
+        cat2.setId(2L);
+        cat2.setNome("Escritório");
+
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+
+        return categorias;
     }
 
 }
